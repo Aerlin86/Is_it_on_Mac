24cdab8abc9c04029026341b46b4cebe92b3aefd
Tom <aerlin86@gmail.com>
2021-11-28 17:19:18 +0100

    v.0.1.4a search working on Steam and GOG, placeholder for other platforms

--- app.py
+++ app.py
@@ -19,24 +19,47 @@ def hello():
 
     # VARIABLE INPUT FROM HTML PAGE
 
-    pp = request.form['game']
-    link_input = pp
+    link_input = request.form['game']
 
-    # ASKING FOR GAME NAME AND CREATING LINK FOR SITE SCRAPING
+    # ASKING FOR GAME NAME AND CREATING LINK FOR SITE SCRAPING || SITE SCRAPING
 
+    # RESULTS
+
+    steam_answer = "NO INFORMATION"
+    gog_answer = "NO INFORMATION"
+    origin_answer = "NO INFORMATION"
+    epic_answer = "NO INFORMATION"
+    uplay_answer = "NO INFORMATION"
+
+    # STEAM
     query = "steam" + link_input
 
     list_of_sites = search(query, num_results=1, lang="en")
     steam_site = list_of_sites[0]
 
-    # SITE SCRAPING
-
     page = requests.get(steam_site)
     soup = BeautifulSoup(page.content, "html.parser")
     result = soup.find("div", class_="sysreq_tabs")
     if result:
-        return render_template("yes.html", game=request.form['game'])
-    return render_template("no.html", game=request.form['game'])
+        steam_answer = "YES"
+    else:
+        steam_answer = "NO"
+
+    # GOG
+    query = "gog" + link_input
+
+    list_of_sites = search(query, num_results=1, lang="en")
+    steam_site = list_of_sites[0]
+
+    page = requests.get(steam_site)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result = soup.find("div", class_="details__content table__row-content")
+    if result:
+        gog_answer = "YES"
+    else:
+        gog_answer = "NO"
+
+    return render_template('result.html', steam=steam_answer, gog=gog_answer, origin=origin_answer, epic=epic_answer, uplay=uplay_answer)
 
 
 if __name__ == "__main__":
