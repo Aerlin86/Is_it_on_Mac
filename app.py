b3fea064e93c2731a37473bb02a847d4cf26e5b0
Tom <aerlin86@gmail.com>
2021-11-29 20:21:48 +0100

    v.0.1.5 re-written result page from table to div and search done for 4 out of 5 services, also more CSS stuff

--- app.py
+++ app.py
@@ -59,6 +59,40 @@ def hello():
     else:
         gog_answer = "NO"
 
+    # ORIGIN
+    query = "ORIGIN" + link_input
+
+    list_of_sites = search(query, num_results=1, lang="en")
+    steam_site = list_of_sites[0]
+    print(steam_site)
+
+    page = requests.get(steam_site)
+    soup = BeautifulSoup(page.content, "html.parser")
+    #result = soup.body.findAll(text="Mac OS")
+    result = soup.body.find("div", string="Mac")
+    print(result)
+    if result:
+        origin_answer = "YES"
+    else:
+        origin_answer = "NO"
+
+    # EPIC
+    query = "EPIC GAME STORE" + link_input
+
+    list_of_sites = search(query, num_results=1, lang="en")
+    steam_site = list_of_sites[0]
+
+    page = requests.get(steam_site)
+    soup = BeautifulSoup(page.content, "html.parser")
+    result = soup.find("div", class_="css-13efb1d")
+    if result:
+        epic_answer = "YES"
+    else:
+        epic_answer = "NO"
+
+    # UPLAY
+    uplay_answer = "NO"
+
     return render_template('result.html', steam=steam_answer, gog=gog_answer, origin=origin_answer, epic=epic_answer, uplay=uplay_answer)
 
 
