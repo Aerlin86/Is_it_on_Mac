3a3f8850857fa9a6ab0f35eae672ede8693cdf78
Tom <aerlin86@gmail.com>
2021-12-04 19:21:22 +0100

    v.0.1.5b more code clean-ups, Orgini still not working, probably will seek help on Stackoverflow...

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ def home():
 @app.route('/hello', methods=['POST', 'GET'])
 def hello():
 
+    # STANDARD ANSWERS
+    steam_answer = "NO"
+    gog_answer = "NO"
+    origin_answer = "NO"
+    epic_answer = "NO"
+    uplay_answer = "NO"
+
     # VARIABLE INPUT FROM HTML PAGE
 
     link_input = request.form['game']
@@ -26,7 +33,7 @@ def hello():
     # STEAM
     query = "steam" + link_input
 
-    list_of_sites = search(query, num_results=1, lang="en")
+    list_of_sites = search(query, num_results=0, lang="en")
     steam_site = list_of_sites[0]
 
     page = requests.get(steam_site)
@@ -34,13 +41,11 @@ def hello():
     result = soup.find("div", class_="sysreq_tabs")
     if result:
         steam_answer = "YES"
-    else:
-        steam_answer = "NO"
 
     # GOG
     query = "gog" + link_input
 
-    list_of_sites = search(query, num_results=1, lang="en")
+    list_of_sites = search(query, num_results=0, lang="en")
     gog_site = list_of_sites[0]
 
     page = requests.get(gog_site)
@@ -48,30 +53,25 @@ def hello():
     result = soup.find("div", class_="details__content table__row-content")
     if result:
         gog_answer = "YES"
-    else:
-        gog_answer = "NO"
 
     # ORIGIN
+    # TODO STILL NOT WORKING
     query = "ORIGIN" + link_input
 
-    list_of_sites = search(query, num_results=1, lang="en")
+    list_of_sites = search(query, num_results=0, lang="en")
     origin_site = list_of_sites[0]
-    print(origin_site)
 
     page = requests.get(origin_site)
     soup = BeautifulSoup(page.content, "html.parser")
-    #result = soup.body.findAll(text="Mac OS")
-    result = soup.body.find("div", string="Mac").getText()
+    result = soup.find("div", class_="origin-store-gdp-header-block")
     print(result)
     if result:
         origin_answer = "YES"
-    else:
-        origin_answer = "NO"
 
     # EPIC
     query = "EPIC GAME STORE" + link_input
 
-    list_of_sites = search(query, num_results=1, lang="en")
+    list_of_sites = search(query, num_results=0, lang="en")
     epic_site = list_of_sites[0]
 
     page = requests.get(epic_site)
@@ -79,11 +79,6 @@ def hello():
     result = soup.find("div", class_="css-13efb1d")
     if result:
         epic_answer = "YES"
-    else:
-        epic_answer = "NO"
-
-    # UPLAY
-    uplay_answer = "NO"
 
     return render_template('result.html', steam=steam_answer, gog=gog_answer, origin=origin_answer, epic=epic_answer, uplay=uplay_answer)
 
